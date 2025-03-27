23fd30f189fb77b16e6eca10c796059f68ca73db
Niaiko22 <ralaivaoniaiko@gmail.com>
2025-03-28 00:57:08 +0300

    Astro API + Dynamic Routes + Index Page + RSS package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://astro-getting-started-test-7a8623.netlify.app/"
+});
